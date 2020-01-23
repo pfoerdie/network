7e4bf41f94dec7e9c1e47a2a54351e80613cc2ca
Simon Petrac <pfoerdie@gmail.com>
2020-01-23 21:14:37 +0100

    interesting idea for the neurons

--- _idea03/Neuron.js
+++ _idea03/Neuron.js
@@ -1,11 +1,17 @@
 const _ = require("../tools.js");
 const _module = require("./module.js");
+const _decay = 0.01, _delay = 10;
+
+function decay(value, dtime) {
+    return value / Math.exp(_decay * dtime);
+}
 
 class Neuron extends _module.Node {
 
     constructor(bias = 0, value = 0) {
         _.assert(_.is.number(bias), "bias is not a number");
         _.assert(_.is.number(value), "value is not a number");
+        let value_ts = Date.now();
         const node_data = Object.create({}, {
             bias: {
                 enumerable: true,
@@ -17,12 +23,26 @@ class Neuron extends _module.Node {
                 }
             },
             value: {
-                enumerable: true,
+                enumerable: false,
                 get() {
+                    let ts = Date.now();
+                    value = decay(value, ts - value_ts);
+                    value_ts = ts;
                     return value;
                 }, set(val) {
                     _.assert(_.is.number(val), "value is not a number");
-                    value = val;
+                    let ts = Date.now();
+                    value = val - value + decay(value, ts - value_ts);
+                    value_ts = ts;
+                }
+            },
+            active: {
+                enumerable: false,
+                get() {
+                    let ts = Date.now();
+                    value = decay(value, ts - value_ts);
+                    value_ts = ts;
+                    return value > bias;
                 }
             }
         });
@@ -46,17 +66,29 @@ class Neuron extends _module.Node {
         return super.attach(neuron, edge_data);
     }
 
-    increase(amount) {
+    activate(amount) {
         _.assert(_.is.number(amount), "amount is not a number");
         this.data.value += amount;
-    }
-
-    activate(activation = Neuron.SIGMOID) {
-        _.assert([Neuron.SIGMOID, Neuron.TANH].includes(activation), "activation is not a valid activation function");
-        for (let edge of this.outputs) {
-            edge.target.increase(edge.data.weight * activation(this.data.value - this.data.bias));
+        if (this.data.active) {
+            this.data.value -= this.data.bias;
+            for (let edge of this.outputs) {
+                // edge.target.activate(edge.data.weight);
+                setTimeout(() => edge.target.activate(edge.data.weight), _delay);
+            }
         }
     }
+
+    // increase(amount) {
+    //     _.assert(_.is.number(amount), "amount is not a number");
+    //     this.data.value += amount;
+    // }
+
+    // activate(activation = Neuron.SIGMOID) {
+    //     _.assert([Neuron.SIGMOID, Neuron.TANH].includes(activation), "activation is not a valid activation function");
+    //     for (let edge of this.outputs) {
+    //         edge.target.increase(edge.data.weight * activation(this.data.value - this.data.bias));
+    //     }
+    // }
 
     static SIGMOID(val) {
         _.assert(_.is.number(val), "val is not a number");
--- _idea03/Neuron.test.js
+++ _idea03/Neuron.test.js
@@ -14,22 +14,22 @@ function getRandom(min = 0, max = 1) {
 }
 
 for (let i = 0; i < input_count; i++) {
-    neural_net.addInput(new Neuron(getRandom(0, 1), getRandom(0, 1)));
+    neural_net.addInput(new Neuron(getRandom(0, 1), 0));
 }
 
 for (let i = 0; i < output_count; i++) {
-    neural_net.addOutput(new Neuron(getRandom(0, 1), getRandom(0, 0)));
+    neural_net.addOutput(new Neuron(getRandom(0, 1), 0));
 }
 
 for (let i = 0; i < middle_count; i++) {
-    let middle_node = new Neuron(getRandom(0, 1), getRandom(0, 0));
+    let middle_node = new Neuron(getRandom(0, 1), 0);
     middle_layer.add(middle_node);
     neural_net.cacheNode(middle_node);
     for (let input of neural_net.inputs) {
-        let input_edge = input.attach(middle_node, getRandom(-.5, .5));
+        let input_edge = input.attach(middle_node, getRandom(-.2, .6));
     }
     for (let output of neural_net.outputs) {
-        let output_edge = middle_node.attach(output, getRandom(-.5, .5));
+        let output_edge = middle_node.attach(output, getRandom(-.2, .6));
     }
 }
 
@@ -37,23 +37,27 @@ for (let i = 0; i < middle_count; i++) {
 
 //#region process nodes with a basic activation function
 
-const ACTIVATION = Neuron.TANH;
-
-function trimValue(value) {
-    // return value;
-    // return Math.round(1e2 * value) / 1e2;
-    return Math.round(value);
-}
-
 function nodesToStr(iterator) {
-    return Array.from(iterator).map(node => trimValue(ACTIVATION(node.data.value - node.data.bias))).join(" ");
+    return Array.from(iterator).map(node => node.data.active ? "1" : "0").join(" ");
 }
 
-for (let input_node of neural_net.inputs) { input_node.activate(ACTIVATION); }
-for (let middle_node of middle_layer) { middle_node.activate(ACTIVATION); }
+function printNet(time = 0) {
+    if (time) console.log("________________________________________________________________________________");
+    console.log(
+        "TIME:\t\t" + time + " s" +
+        "\ninputs:\t\t" + nodesToStr(neural_net.inputs) +
+        "\nmiddle_layer:\t" + nodesToStr(middle_layer) +
+        "\noutput:\t\t" + nodesToStr(neural_net.outputs)
+    );
+}
 
-console.log("inputs:\t\t" + nodesToStr(neural_net.inputs));
-console.log("middle_layer:\t" + nodesToStr(middle_layer));
-console.log("output:\t\t" + nodesToStr(neural_net.outputs));
+for (let input_node of neural_net.inputs) {
+    input_node.activate(Math.random());
+}
+
+for (let delay of [0, .002, .01, .05, .1, .2, .3, .5, 1, 2]) {
+    if (delay) setTimeout(printNet, delay * 1e3, delay);
+    else printNet(delay);
+}
 
 //#endregion
